683877d295d20501ee5f2e4009b2d6231a0af0b9
TasyaV <tasya-vas@mail.ru>
2021-02-13 21:45:07 +0300

    Initial Commit

---
+++ VKNewsFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VKNewsFeed
+//
+//  Created by tasya on 13.02.2021.
+//  Copyright © 2021 Taisiya V. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
